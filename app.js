a7e7c25fe4abfa9bf95cdc6e5aa3209cc99c7955
Alqurtubi17 <mudin1487@gmail.com>
2024-05-03 12:45:03 +0700

    update materi 3

--- app.js
+++ app.js
@@ -42,6 +42,18 @@ app.get('/materi-3', (req, res) => {
     res.render('materi-3')
 })
 
+// Handle requests for specific HTML files inside the 'materi-3' folder
+app.get('/materi-3/:fileName', (req, res) => {
+    const fileName = req.params.fileName;
+    res.render(`materi-3/${fileName}`, (err, html) => {
+        if (err) {
+            console.error(err); // Menampilkan kesalahan jika terjadi kesalahan saat merender file
+            return res.status(500).send('Internal Server Error'); // Memberikan respons 500 jika terjadi kesalahan saat merender file
+        }
+        res.send(html); // Mengirimkan HTML jika berhasil
+    });
+});
+
 app.get('/materi-4', (req, res) => {
     res.render('materi-4')
 })
